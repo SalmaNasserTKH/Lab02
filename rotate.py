1cc0a9deef0cbdff464ea9f3fab04f557335d076
SalmaNasserTKH <99175172+SalmaNasserTKH@users.noreply.github.com>
2022-02-13 23:36:14 +0200

    Create rotate.py

---
+++ rotate.py
@@ -0,0 +1,48 @@
+import rospy
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Point, Twist
+from math import atan2,sqrt, pow , pi 
+
+x = 0.0
+y = 0.0 
+theta = 0.0
+
+
+def newOdom(msg):
+    global x
+    global y
+    global theta
+    
+
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+
+    rot_q = msg.pose.pose.orientation
+    (roll, pitch, theta) = euler_from_quaternion([ rot_q.x, rot_q.y, rot_q.z, rot_q.w ])
+   
+rospy.init_node("speed_controller")
+
+sub = rospy.Subscriber("/odom", Odometry, newOdom)
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+
+velocity = Twist()
+r = rospy.Rate(4)
+
+goal = Point()
+goal.x = 3
+goal.y = 3
+
+while not rospy.is_shutdown(): 
+    angle_to_goal = atan2(goal.y, goal.x)
+    # angle_to_goal_degrees = angle_to_goal *180/pi
+    # print("angle to goal: ", angle_to_goal_degrees)
+    abs(angle_to_goal - theta) > 0.1
+    if abs(angle_to_goal - theta) > 0.1: 
+        velocity.angular.z = 0.3
+    else:
+        velocity.angular.z = 0.0
+
+
+    pub.publish(velocity)
+    r.sleep()    
